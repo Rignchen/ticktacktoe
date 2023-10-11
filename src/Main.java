8158a75074cef2f6aacbb43653e08c4e641794d9
Rignchen <rinchendemort@gmail.com>
2023-10-11 10:41:08 +0200

    change name and try to make clear function

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,7 +13,7 @@ public class Main {
             tableau.add(row);
         }
         //make a copy of the board called trigger, except all values are in the main list (will be used to check which case can be played)
-        var trigger = tableau.stream().flatMap(List::stream).toList();
+        var trigger = tableau.stream().flatMap(List::stream).collect(Collectors.toList());
         //init some other variables
         boolean is_playing = true;
         short turn = 0;
@@ -23,42 +24,36 @@ public class Main {
             //todo
         }
     }
-    public static void printBoard(List<List<Character>> tableau) {
+    public static void clear() {
+        //clear the shell
+        try {Runtime.getRuntime().exec("clear");}
+        catch (Exception e) {e.printStackTrace();}
+    }
+    public static String getBoard(List<List<Character>> tableau) {
     /*  X|X|X
         -+-+-
         X|X|X
         -+-+-
         X|X|X   */
-        for (int i = 0; i < tableau.size(); i++) {
-            for (int j = 0; j < tableau.get(i).size(); j++) {
-                System.out.print(tableau.get(i).get(j));
-                if (j < tableau.get(i).size() - 1) {
-                    System.out.print("|");
-                }
-            }
-            System.out.println();
-            if (i < tableau.size() - 1) {
-                for (int j = 0; j < tableau.get(i).size(); j++) {
-                    System.out.print("-");
-                    if (j < tableau.get(i).size() - 1) {
-                        System.out.print("+");
-                    }
-                }
-                System.out.println();
-            }
-        }
+        return tableau.stream()
+                .map(row -> row.stream()
+                        .map(String::valueOf)
+                        .reduce("", (a, b) -> a + "|" + b)
+                        .substring(1))
+                .reduce("", (a, b) -> a + "\n-+-+-\n" + b)
+                .substring(1);
     }
-    public static String Input(String text) {
+    public static String input(String text) {
         //Print the text, ask the user an input, return the input
         Scanner sc = new Scanner(System.in);
         System.out.print(text);
         return sc.nextLine();
     }
-    public static Character InputIn(String text, List<Character> values) {
+    public static Character inputIn(String text, List<Character> values) {
         //get an input from the user, check if the input is in values, return the input if it is, else ask again
-        String input = Input(text);
+        String input = input(text);
         while (!values.contains(input.charAt(0))) {
-            input = Input(text);
+            input = input(text);
         }
         return input.charAt(0);
     }
