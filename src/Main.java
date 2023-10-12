a15d24f54389e16f0ecf4e88fc2d63b5abbcfda7
Rignchen <rinchendemort@gmail.com>
2023-10-12 13:34:20 +0200

    Allow player to play again

--- src/Main.java
+++ src/Main.java
@@ -5,46 +5,11 @@ import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Main {
-    static boolean is_playing = true;
-    static short turn = 0;
-    static boolean player1won = false;
-    static boolean player2won = false;
-    public static void main(String[] args) {
-        //generate a list of list of character
-        List<List<Character>> tableau = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            List<Character> row = new ArrayList<>();
-            for (int j = 0; j < 3; j++) {row.add((char) (3*i + j + '1'));}
-            tableau.add(row);
-        }
-        //make a copy of the board called trigger, except all values are in the main list (will be used to check which case can be played)
-        var trigger = tableau.stream().flatMap(List::stream).collect(Collectors.toList());
-        //init some other variables
-        //main loop
-        while (is_playing) {
-            //print the board and ask the player to play
-            clear();
-            System.out.println(getBoard(tableau));
-            var played = inputIn("Player " + (turn % 2 + 1) + " turn: ", trigger);
-            //remove the value played from the trigger list so it can't be played again
-            trigger.remove(played);
-            //update the board
-            var row = (played - '1') / 3;
-            var col = (played - '1') % 3;
-            tableau.get(row).set(col, (turn % 2 == 0) ? 'X' : 'O');
-            //check if the player won
-            end(tableau);
-            //update some variables
-            turn++;
-            if (turn == 9) {is_playing = false;}
-        }
-        clear();
-        System.out.println(getBoard(tableau));
-        System.out.println();
-        if (player1won) {System.out.println("Player 1 won");}
-        else if (player2won) {System.out.println("Player 2 won");}
-        else {System.out.println("Tie, nobody won");}
-    }
+    static boolean is_playing;
+    static short turn;
+    static boolean player1won;
+    static boolean player2won;
+    public static void main(String[] args) {do {play();} while (inputIn("Do you want to play again? [y/n]",List.of('y','n')) == 'y');}
     public static void clear() {
         //clear the shell
         for (int i = 0; i<50; i++) {System.out.println();}
@@ -80,8 +45,8 @@ public class Main {
             if (tableau.get(i).get(0) == tableau.get(i).get(1) && tableau.get(i).get(1) == tableau.get(i).get(2)) {win(tableau.get(i).get(1));}
             if (tableau.get(0).get(i) == tableau.get(1).get(i) && tableau.get(1).get(i) == tableau.get(2).get(i)) {win(tableau.get(1).get(i));}
             if (
-                (tableau.get(1).get(1) == tableau.get(0).get(0) && tableau.get(1).get(1) == tableau.get(2).get(2)) ||
-                (tableau.get(1).get(2) == tableau.get(0).get(0) && tableau.get(1).get(1) == tableau.get(2).get(1))
+                (tableau.get(0).get(0) == tableau.get(1).get(1) && tableau.get(1).get(1) == tableau.get(2).get(2)) ||
+                (tableau.get(0).get(2) == tableau.get(1).get(1) && tableau.get(1).get(1) == tableau.get(2).get(0))
             ) {win(tableau.get(1).get(1));}
         }
     }
@@ -91,5 +56,45 @@ public class Main {
             case 'O' -> player2won = true;
         }
         is_playing = false;
+    }
+    private static void play() {
+        //generate a list of list of character
+        List<List<Character>> tableau = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            List<Character> row = new ArrayList<>();
+            for (int j = 0; j < 3; j++) {row.add((char) (3*i + j + '1'));}
+            tableau.add(row);
+        }
+        //make a copy of the board called trigger, except all values are in the main list (will be used to check which case can be played)
+        var trigger = tableau.stream().flatMap(List::stream).collect(Collectors.toList());
+        //init some other variables
+        is_playing = true;
+        turn = 0;
+        player1won = false;
+        player2won = false;
+        //main loop
+        while (is_playing) {
+            //print the board and ask the player to play
+            clear();
+            System.out.println(getBoard(tableau));
+            var played = inputIn("Player " + (turn % 2 + 1) + " turn: ", trigger);
+            //remove the value played from the trigger list so it can't be played again
+            trigger.remove(played);
+            //update the board
+            var row = (played - '1') / 3;
+            var col = (played - '1') % 3;
+            tableau.get(row).set(col, (turn % 2 == 0) ? 'X' : 'O');
+            //check if the player won
+            end(tableau);
+            //update some variables
+            turn++;
+            if (turn == 9) {is_playing = false;}
+        }
+        clear();
+        System.out.println(getBoard(tableau));
+        System.out.println();
+        if (player1won) {System.out.println("Player 1 won");}
+        else if (player2won) {System.out.println("Player 2 won");}
+        else {System.out.println("Tie, nobody won");}
     }
 }
