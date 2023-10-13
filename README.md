7faffc32bfc3742cd346e5423fb7e4a6a954ba50
Rignchen <rinchendemort@gmail.com>
2023-10-13 09:48:44 +0200

    Add Readme

---
+++ README.md
@@ -0,0 +1,16 @@
+# Tick Tack Toe
+
+## What's a Tick Tack Toe?
+Tick Tack Toe is a game where two players take turns marking spaces in a 3x3 grid. The player who succeeds in placing three of their marks in a straight line wins the game, this includes horizontal, vertical and diagonal lines.
+
+## How to play
+1. Find a friend to play with (or play against yourself)
+2. Clone this repository
+3. Open the terminal and navigate to the repository folder
+4. Run the following command: 'java ./src/Main.java'
+5. Enter the slot number you want to place your mark in
+6. Player 1 is 'X' and Player 2 is 'O'
+7. The first player to get three of their marks in a straight line wins the game
+8. If all 9 slots are filled and no player has three of their marks in a straight line, the game is a draw
+9. Once the game is over, you can choose to play again or exit the game
+10. Enjoy!
--- src/Main.java
+++ src/Main.java
@@ -9,11 +9,20 @@ public class Main {
     static short turn;
     static boolean player1won;
     static boolean player2won;
-    public static void main(String[] args) {do {play();} while (inputIn("Do you want to play again? [y/n]",List.of('y','n')) == 'y');}
+
+    public static void main(String[] args) {
+        do {
+            play();
+        } while (inputIn("Do you want to play again? [y/n]", List.of('y', 'n')) == 'y');
+    }
+
     public static void clear() {
         //clear the shell
-        for (int i = 0; i<50; i++) {System.out.println();}
+        for (int i = 0; i < 50; i++) {
+            System.out.println();
+        }
     }
+
     public static String getBoard(List<List<Character>> tableau) {
     /*  X|X|X
         -+-+-
@@ -28,28 +37,40 @@ public class Main {
                 .reduce("", (a, b) -> a + "\n-+-+-\n" + b)
                 .substring("\n-+-+-\n".length());
     }
+
     public static String input(String text) {
         //Print the text, ask the user an input, return the input
         Scanner sc = new Scanner(System.in);
         System.out.print(text);
         return sc.nextLine();
     }
+
     public static Character inputIn(String text, List<Character> values) {
         //get an input from the user, check if the input is in values, return the input if it is, else ask again
         String input;
-        do {input = input(text);} while (Objects.equals(input, "") || !values.contains(input.charAt(0)));
+        do {
+            input = input(text);
+        } while (Objects.equals(input, "") || !values.contains(input.charAt(0)));
         return input.charAt(0);
     }
+
     private static void end(List<List<Character>> tableau) {
-        for (int i=0; i<3; i++) {
-            if (tableau.get(i).get(0) == tableau.get(i).get(1) && tableau.get(i).get(1) == tableau.get(i).get(2)) {win(tableau.get(i).get(1));}
-            if (tableau.get(0).get(i) == tableau.get(1).get(i) && tableau.get(1).get(i) == tableau.get(2).get(i)) {win(tableau.get(1).get(i));}
-            if (
+        for (int i = 0; i < 3; i++) {
+            if (tableau.get(i).get(0) == tableau.get(i).get(1) && tableau.get(i).get(1) == tableau.get(i).get(2)) {
+                win(tableau.get(i).get(1));
+            }
+            if (tableau.get(0).get(i) == tableau.get(1).get(i) && tableau.get(1).get(i) == tableau.get(2).get(i)) {
+                win(tableau.get(1).get(i));
+            }
+        }
+        if (
                 (tableau.get(0).get(0) == tableau.get(1).get(1) && tableau.get(1).get(1) == tableau.get(2).get(2)) ||
-                (tableau.get(0).get(2) == tableau.get(1).get(1) && tableau.get(1).get(1) == tableau.get(2).get(0))
-            ) {win(tableau.get(1).get(1));}
+                        (tableau.get(0).get(2) == tableau.get(1).get(1) && tableau.get(1).get(1) == tableau.get(2).get(0))
+        ) {
+            win(tableau.get(1).get(1));
         }
     }
+
     private static void win(char value) {
         switch (value) {
             case 'X' -> player1won = true;
@@ -57,12 +78,15 @@ public class Main {
         }
         is_playing = false;
     }
+
     private static void play() {
         //generate a list of list of character
         List<List<Character>> tableau = new ArrayList<>();
         for (int i = 0; i < 3; i++) {
             List<Character> row = new ArrayList<>();
-            for (int j = 0; j < 3; j++) {row.add((char) (3*i + j + '1'));}
+            for (int j = 0; j < 3; j++) {
+                row.add((char) (3 * i + j + '1'));
+            }
             tableau.add(row);
         }
         //make a copy of the board called trigger, except all values are in the main list (will be used to check which case can be played)
@@ -88,13 +112,19 @@ public class Main {
             end(tableau);
             //update some variables
             turn++;
-            if (turn == 9) {is_playing = false;}
+            if (turn == 9) {
+                is_playing = false;
+            }
         }
         clear();
         System.out.println(getBoard(tableau));
         System.out.println();
-        if (player1won) {System.out.println("Player 1 won");}
-        else if (player2won) {System.out.println("Player 2 won");}
-        else {System.out.println("Tie, nobody won");}
+        if (player1won) {
+            System.out.println("Player 1 won");
+        } else if (player2won) {
+            System.out.println("Player 2 won");
+        } else {
+            System.out.println("Tie, nobody won");
+        }
     }
 }
